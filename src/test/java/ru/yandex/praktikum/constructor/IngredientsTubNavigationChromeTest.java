df723b6ef257ba5f5f04cb97ebc7843e7aea4659
Svetlana Isaeva <s.isaeva2020@yandex.ru>
2022-03-31 20:05:44 +0300

    changes have been made to the tests IngredientsTubNavigationChromeTest, IngredientsTubNavigationYBTest

---
+++ src/test/java/ru/yandex/praktikum/constructor/IngredientsTubNavigationChromeTest.java
@@ -0,0 +1,39 @@
+package ru.yandex.praktikum.constructor;
+
+import io.qameta.allure.Step;
+import org.junit.Test;
+import ru.yandex.praktikum.BaseTestChrome;
+import ru.yandex.praktikum.MainPage;
+
+import static com.codeborne.selenide.Selenide.open;
+import static org.junit.Assert.assertTrue;
+
+public class IngredientsTubNavigationChromeTest extends BaseTestChrome {
+    @Test
+    @Step("Click to Sauce Tub,  Click to Bun Tub")
+    public void successfulBunTubNavigation() {
+
+        MainPage mainPage = open(MainPage.URL, MainPage.class);
+        mainPage.clickSauceTub();
+        mainPage.clickBunTub();
+        assertTrue(mainPage.isFluorBunPictDisplayed());
+    }
+
+    @Test
+    @Step("Click to Bun Tub,  Click to Sauce Tub")
+    public void successfulSauceTubNavigation() {
+        MainPage mainPage = open(MainPage.URL, MainPage.class);
+        mainPage.clickBunTub();
+        mainPage.clickSauceTub();
+        assertTrue(mainPage.isSpicyXSaucePictDisplayed());
+    }
+
+    @Test
+    @Step("Click to Bun Tub,  Click to Filling Tub")
+    public void successfulFillingTubNavigation() {
+        MainPage mainPage = open(MainPage.URL, MainPage.class);
+        mainPage.clickBunTub();
+        mainPage.clickFillingTub();
+        assertTrue(mainPage.isProtostomiaMeatPictDisplayed());
+    }
+}
